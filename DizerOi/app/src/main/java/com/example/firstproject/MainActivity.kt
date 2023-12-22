9bb4f4e61c14136e59d7c31eaf26c1f7dc4ded2b
gregorio <igregoriodoneto@outlook.com>
2023-12-22 10:06:41 -0300

    feat: Acrescentando o Toast e tratamento no input

--- DizerOi/app/src/main/java/com/example/firstproject/MainActivity.kt
+++ DizerOi/app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firstproject
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firstproject.databinding.ActivityMainBinding
 
@@ -13,8 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonOla.setOnClickListener {
-            val nome = binding.editNome.text.toString()
-            binding.textResultado.setText("Olá $nome")
+            val nome = binding.editNome.text.toString().trim()
+            val sobrenome = binding.editSobrenome.text.toString().trim()
+
+            if (nome.isEmpty() || sobrenome.isEmpty()) {
+                binding.textResultado.text = "Nome/Sobrenome inválido!"
+                Toast.makeText(applicationContext, "Nome/Sobrenome inválido!", Toast.LENGTH_SHORT).show()
+            } else {
+                binding.textResultado.setText("Olá $nome $sobrenome")
+                Toast.makeText(applicationContext, "Olá $nome $sobrenome", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
