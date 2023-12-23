52ae66692300192a75f9277cd6c679b63ee3ca54
gregorio <igregoriodoneto@outlook.com>
2023-12-23 10:53:25 -0300

    feat: Criando uma simulação de Login

---
+++ LoginApp/app/src/main/java/com/greg/loginapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.greg.loginapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.greg.loginapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonEntrar.setOnClickListener {
+            var username = binding.editUsername.text.toString().trim()
+            var password = binding.editPassword.text.toString().trim()
+
+            if (username.equals("user") && password.equals("pass")) {
+                Toast.makeText(applicationContext, "Login efetuado com sucesso!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Login Inválido!", Toast.LENGTH_SHORT).show()
+            }
+
+            binding.editUsername.setText("")
+            binding.editPassword.setText("")
+        }
+    }
+}
